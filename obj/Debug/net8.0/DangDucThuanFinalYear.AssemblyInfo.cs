0e095b5f5c7024accb573d9610f51bccba5f47f0
DangDucThuan38 <dangducthuan999@gmail.com>
2024-04-10 22:35:39 +0700

    Update

--- obj/Debug/net8.0/DangDucThuanFinalYear.AssemblyInfo.cs
+++ obj/Debug/net8.0/DangDucThuanFinalYear.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DangDucThuanFinalYear")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6778db69e2a25daae7610fcc2544e47bd379af2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7e1961e2841f679ff301488adc863be3edae85")]
 [assembly: System.Reflection.AssemblyProductAttribute("DangDucThuanFinalYear")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DangDucThuanFinalYear")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
